777eb15ed9a7933ee298b4dc93e34aef5ad5e9f9
donderet <148077412+donderet@users.noreply.github.com>
2024-06-03 00:14:39 +0300

    Hide console window for release builds

--- roser/Roser.cs
+++ roser/Roser.cs
@@ -1,7 +1,9 @@
-﻿using roser;
+﻿using Microsoft.Win32.SafeHandles;
+using roser;
 using roser.native;
 using roser.scenes;
 using System.Runtime.InteropServices;
+using System.Text;
 
 internal partial class Roser
 {
@@ -9,9 +11,27 @@ internal partial class Roser
 	[return: MarshalAs(UnmanagedType.Bool)]
 	public static partial bool SetProcessDpiAwareness(ProcessDpiAwareness awareness);
 
+#if DEBUG
+	[LibraryImport("kernel32.dll", SetLastError = true)]
+	public static partial IntPtr GetStdHandle(int nStdHandle);
+
+	[LibraryImport("kernel32.dll", SetLastError = true)]
+	public static partial int AllocConsole();
+#endif
+
 	private static void Main(string[] args)
 	{
 #if DEBUG
+		AllocConsole();
+		IntPtr stdHandle = GetStdHandle(-11);
+		SafeFileHandle safeFileHandle = new(stdHandle, true);
+		FileStream fileStream = new(safeFileHandle, FileAccess.Write);
+		StreamWriter standardOutput = new(fileStream)
+		{
+			AutoFlush = true
+		};
+		Console.SetOut(standardOutput);
+
 		ConsoleListener.Start();
 #endif
 		SetProcessDpiAwareness(ProcessDpiAwareness.ProcessPerMonitorDpiAware);
